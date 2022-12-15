64a10023992f56480d9f7eb75852ce204727702e
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-15 23:35:11 +0500

    add Example013_Recursion_factorial

---
+++ Example013_Recursion_factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления факторил 3. Использовать рекурсию.
+
+double Factorial(int n) // ! быстрорастущая функция, поэтому используем
+{                       // тип данных double
+   // 1! = 1
+   // 0! = 1
+   if (n ==1) return 1;
+   else return n * Factorial(n-1);
+}
+    for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+  
