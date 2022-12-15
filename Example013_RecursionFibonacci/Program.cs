b5801ddae315b86fd06b7e108de165eb3851ed71
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-15 23:50:41 +0500

    add Example013_RecursionFibonacci

---
+++ Example013_RecursionFibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти число Фибоначчи равное 50
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+   if(n == 1 || n == 2) return 1;
+   else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
+// начиная с 43 числа программа начинает медленно считать
+// причину рассмотрим на семинаре
+
