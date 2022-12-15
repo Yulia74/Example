7cbf09b4ebd0f4572bd0040b1577a7093e3bc916
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-15 22:09:37 +0500

    add Example013_RecursionAlgorithm_string

---
+++ Example013_RecursionAlgorithm_string/Program.cs
@@ -0,0 +1,15 @@
+﻿string[,] table = new string[2, 5];
+// String.Empty       по умолчанию тип данных строка - строки пустые
+// table[0,0]  table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// table[1,0]  table[1,1]  table[1,2]  table[1,3]  table[1,4]
+
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int colums = 0; colums < 5; colums++)
+    {
+        Console.WriteLine($"-{table[rows, colums]}-");
+    }
+}
+
