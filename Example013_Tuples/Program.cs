68d5717e6cbe1b7649bd98409131ade2f8a41777
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-17 00:50:12 +0500

    add Tuples

---
+++ Example013_Tuples/Program.cs
@@ -0,0 +1,13 @@
+﻿// Tuples - кортежи, позволяют вывести из метода несколько значений
+// и разных типов переменных (int, double, object)
+
+(int,int) coordinates = GetSomething();
+Console.WriteLine(coordinates.Item1);
+Console.WriteLine(coordinates.Item2);
+
+(int,int) GetSomething()
+{
+    int a = 5;
+    int b = 10;
+    return (a,b);
+}
