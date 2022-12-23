017270e7a0aa088e247230475b38987a1c728b51
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-23 21:17:39 +0500

    add Example014_RecursionString

---
+++ Example014_RecursionString/Program.cs
@@ -0,0 +1,28 @@
+﻿// Собрать строку с числами от а до b (a <= b).
+
+// итеративный подход
+
+Console.WriteLine(NumbersFor(1, 10));
+
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+
+// с помощью рекурсии
+
+Console.WriteLine(NumbersRec(1, 10));
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b)
+        return $"{a} " + NumbersRec(a + 1, b);
+    else
+        return String.Empty;
+}
