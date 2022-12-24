649df3410866370e43fea1f27cf7a2c390ffe3ed
[Yulia Lebedeva] <yl.75@mail.ru>
2022-12-24 21:56:26 +0500

    Example015_Watch is done

---
+++ Example015_Watch/Program.cs
@@ -0,0 +1,26 @@
+﻿// Использование метода для подсчета скорости работы написанного кода
+
+using System;                             // 1
+using System.Diagnostics;                 // 2
+
+Console.Write("Enter a number - the base of the degree (A):  ");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter a number - exponent (B):  ");
+
+int b = int.Parse(Console.ReadLine());
+
+Stopwatch watch = new Stopwatch();          // 3
+watch.Start();                              // 4
+
+double PowerAB(double a, double b)
+{
+    if (b < 0) return (1 / a) * PowerAB(a, b + 1); // a ^ (-2) = 1 / (a * a)
+    if (b > 0) return a * PowerAB(a, b - 1);
+    else return 1;
+}
+Console.WriteLine($"The number A to the power of B is equal to   {PowerAB(a, b)}");
+
+watch.Stop();                                 // 5
+Console.WriteLine(watch.ElapsedMilliseconds); // 6
