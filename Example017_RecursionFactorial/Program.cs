4a8f8325b0b3f511390856466509712f1b039e47
[Yulia Lebedeva] <yl.75@mail.ru>
2023-01-23 21:38:24 +0500

    add example 017

---
+++ Example017_RecursionFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// Факториал числа
+// итеративный подход
+/*
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
+Console.WriteLine(FactorialFor(10));
+*/
+
+// рекурсия
+
+int Factorialrec(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorialrec(n - 1);
+}
+Console.WriteLine(Factorialrec(10));
