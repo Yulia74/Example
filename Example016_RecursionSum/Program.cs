dd703c2ab63342bc54d5cd62b803392f256255c1
[Yulia Lebedeva] <yl.75@mail.ru>
2023-01-23 21:09:00 +0500

    Add example 016

---
+++ Example016_RecursionSum/Program.cs
@@ -0,0 +1,21 @@
+﻿// Сумма чисел от 1 до n
+// итеративный подход
+/*
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 0; i <= n; i++)
+        result += i;
+    return result;
+}
+Console.WriteLine(SumFor(10));
+*/
+
+// рекусия
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumRec(10));
