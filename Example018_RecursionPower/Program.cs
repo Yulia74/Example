1c841f1d0664a3ed442f48933b4dfed3a95a308b
[Yulia Lebedeva] <yl.75@mail.ru>
2023-01-23 22:11:16 +0500

    Add example 018

---
+++ Example018_RecursionPower/Program.cs
@@ -0,0 +1,34 @@
+﻿// Вычислить а в степени n
+
+// итеративный подход
+/*
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= a;
+    return result;
+}
+Console.WriteLine(PowerFor(2, 10));
+
+
+// рекурсия
+
+int PowerRec(int a, int n)
+{
+    return n == 0 ? 1 : PowerRec(a, n - 1) * a; // синтаксический сахар
+    // if (n == 0) return 1;
+    // else return PowerRec(a, n - 1) * a;
+}
+Console.WriteLine(PowerRec(2, 10));
+*/
+
+// рекурсия, если степень четная
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowerRecMath(2, 10));
